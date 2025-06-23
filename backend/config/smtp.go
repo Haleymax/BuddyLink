77def7a8ae32fd38d2bea7e736bfe87eb36eb530
mac-haley <hongwei.huang42@gmail.com>
2025-06-23 17:23:55 +0800

    add email client

---
+++ backend/config/smtp.go
@@ -0,0 +1,8 @@
+package config
+
+type SMTPConfig struct {
+	Email    string `mapstructure:"email"`
+	Password string `mapstructure:"password"`
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+}
