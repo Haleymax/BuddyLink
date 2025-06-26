cb30526b4e2d8fd1bdc2a1427afd0cf811dbf1fd
mac-haley <hongwei.huang42@gmail.com>
2025-06-26 23:06:05 +0800

    send verification code

--- backend/internal/app/services/stmp_service.go
+++ backend/internal/app/services/stmp_service.go
@@ -1,12 +1,59 @@
 package services
 
-import "buddylink/pkg/email"
+import (
+	"buddylink/config"
+	"buddylink/pkg/cache_client"
+	"buddylink/pkg/email"
+	"log"
+	"math/rand"
+	"time"
+)
 
 type StmpService interface {
-	SendVerification(email string) error
-	CheckVerification(email string) error
+	SendVerification(email string, redis cache_client.RedisClient) error
 }
 
 type StmpServiceImpl struct {
 	Client email.EmailClient
+}
+
+func NewStmpService(cfg *config.SMTPConfig) StmpService {
+
+	stmp_client, err := email.NewEmailClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return &StmpServiceImpl{
+		Client: stmp_client,
+	}
+}
+
+func (s *StmpServiceImpl) SendVerification(email string, redis cache_client.RedisClient) error {
+	verificationCode := generateVerificationCode()
+	subject := "verification code"
+	content := "your verification code is " + verificationCode
+	err := s.Client.SendMail(email, subject, content)
+	if err != nil {
+		log.Printf("Error sending verification email %v", err)
+		return err
+	}
+
+	key := email
+	err = redis.Set(key, []byte(verificationCode), 2*time.Minute)
+	if err != nil {
+		log.Printf("Error sending verification email %v", err)
+		return err
+	}
+	return nil
+}
+
+// 生成验证码
+func generateVerificationCode() string {
+	rand.Seed(time.Now().UnixNano())
+	code := make([]byte, 6)
+	for i := range code {
+		code[i] = byte(rand.Intn(26) + 65)
+	}
+	return string(code)
 }
