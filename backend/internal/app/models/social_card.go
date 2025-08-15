8ba40fabeb73e5ea979f7a0c20ebb4cd4610cccd
haley <p-huanghw@xiaopeng.com>
2025-08-15 15:58:31 +0800

    adjusted the format of cards in the database

--- backend/internal/app/models/social_card.go
+++ backend/internal/app/models/social_card.go
@@ -14,22 +14,22 @@ const (
 
 type SocialCard struct {
 	BaseModel
-	UserID         uint64     `gorm:"index" json:"user_id"`
-	Content        string     `gorm:"type:text;not null" json:"content"`
-	Title          string     `gorm:"size:255;not null" json:"title"`
-	Type           string     `gorm:"size:255;not null" json:"type"`
-	Images         string     `gorm:"type:text" json:"images"`
-	LikesCount     uint64     `gorm:"default:0" json:"likes_count"`
-	ViewsCount     uint64     `gorm:"default:0" json:"views_count"`
-	CommentsCount  uint64     `gorm:"default:0" json:"comments_count"`
-	GenderRequired string     `gorm:"default:all" json:"gender_required"`
-	PeopleRequired uint64     `gorm:"default:0" json:"people_required"`
-	PeopleCount    uint64     `gorm:"default:0" json:"people_count"`
-	IsPrivate      bool       `gorm:"default:false" json:"is_private"`
-	Location       string     `gorm:"size:255" json:"location"`
-	Status         CardStatus `gorm:"type:enum('draft','active','closed','deleted', 'completed');default:'draft'" json:"status"`
-	Date           time.Time  `gorm:"type:timestamp(3);default:CURRENT_TIMESTAMP(3)" json:"date"`
-	Tags           string     `gorm:"type:text" json:"tags"`
+	UserID         uint64                 `gorm:"index" json:"user_id"`
+	Content        string                 `gorm:"type:text;not null" json:"content"`
+	Title          string                 `gorm:"size:255;not null" json:"title"`
+	Type           string                 `gorm:"size:255;not null" json:"type"`
+	Images         string                 `gorm:"type:text" json:"images"`
+	LikesCount     uint64                 `gorm:"default:0" json:"likes_count"`
+	ViewsCount     uint64                 `gorm:"default:0" json:"views_count"`
+	CommentsCount  uint64                 `gorm:"default:0" json:"comments_count"`
+	GenderRequired string                 `gorm:"default:all" json:"gender_required"`
+	PeopleRequired uint64                 `gorm:"default:0" json:"people_required"`
+	PeopleCount    uint64                 `gorm:"default:0" json:"people_count"`
+	IsPrivate      bool                   `gorm:"default:false" json:"is_private"`
+	Location       map[string]interface{} `gorm:"size:255;serializer:json" json:"location"`
+	Status         CardStatus             `gorm:"type:enum('draft','active','closed','deleted', 'completed');default:'draft'" json:"status"`
+	Date           time.Time              `gorm:"type:timestamp(3);default:CURRENT_TIMESTAMP(3)" json:"date"`
+	Tags           []string               `gorm:"type:text;serializer:json" json:"tags"`
 
 	// Relationships
 	User     User      `gorm:"foreignKey:UserID" json:"user"`
