65a583cc720dfff5f7db77f414fe64831dad7334
hongwei.huang <p-huanghw@xiaopeng.com>
2025-08-28 19:58:33 +0800

    compleate message box vue code

--- front/src/model/message.ts
+++ front/src/model/message.ts
@@ -1,9 +1,13 @@
 
 export interface BaseMessage {
+    id: number
+    created_at: string
+    updated_at: string
     sender_id: number
     receiver_id: number
     type: 'apply' | 'comment' | 'like' | 'follow' | 'system'
+    status: string
     data: any
-    isRead: boolean
-    action: 'create' | 'update'
-}
+    action: 'create' | 'update' | 'done'
+    isRead?: boolean
+} 
