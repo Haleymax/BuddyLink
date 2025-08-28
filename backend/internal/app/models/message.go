d6c3352f9cc22e4c0c266ab5dc1847320649aef0
haley <p-huanghw@xiaopeng.com>
2025-08-28 20:00:11 +0800

    add actiom function field

--- backend/internal/app/models/message.go
+++ backend/internal/app/models/message.go
@@ -7,4 +7,5 @@ type Message struct {
 	Type       string                 `gorm:"size:50;not null" json:"type"`
 	Status     string                 `gorm:"size:50;default:'active'" json:"status"`
 	Data       map[string]interface{} `gorm:"size:255;serializer:json" json:"data"`
+	Action     string                 `gorm:"size:20;default:'done'" json:"action"`
 }
